4dbc737fd13835b21f22e3270b7d460d5daa09e9
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 22:37:46 -0600

    amentities

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Amenities view for api v1"""
+
+from flask import jsonify, abort, request
+from models import storage
+from models.amenity import Amenity
+from api.v1.views import app_views
+from datetime import datetime
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def list_amenities():
+    """List all amenities"""
+    amenities = storage.all(Amenity).values()
+    return jsonify([amenity.to_dict() for amenity in amenities])
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    """Get an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    """Create an amenity"""
+    if not request.json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    amenity = Amenity(**request.json)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """Update an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    for key, value in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Delete an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({})
+
+
+if __name__ == '__main__':
+    pass
