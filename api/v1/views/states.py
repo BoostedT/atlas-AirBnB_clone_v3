92b12b3604b34d117c32d279c746fae19fbb682b
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 21:49:54 -0600

    changes to get_state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,11 +19,10 @@ def list_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """ Retrieves a State object """
-    all_states = storage.all(State).values()
-    state_obj = [obj.to_dict() for obj in storage.all(State).values()]
-    if state_obj == []:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    return jsonify(state_obj)
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
