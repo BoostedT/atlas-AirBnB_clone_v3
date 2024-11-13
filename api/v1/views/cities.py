6c2ffce44a707af4974f03d245c8e36ed8ba43c0
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-13 13:44:13 -0600

    made small changes to get method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,12 +11,10 @@ from models.city import City
 @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def list_cities(state_id):
     """Retrieves the list of all City objects of a State"""
-    all_states = storage.all("State").values()
-    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if state_obj == []:
+    state = storage.get("State", state_id)
+    if state is None:
         abort(404)
-    list_cities = [obj.to_dict() for obj in storage.all("City").values()
-                   if state_id == obj.state_id]
+    list_cities = [city.to_dict() for city in state.cities]
     return jsonify(list_cities)
 
 
@@ -59,16 +57,17 @@ def create_city(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
-    """Updates a City object"""
-    city = storage.get(City, city_id)
-    if city is None:
+    """ Updates a city object """
+    state = storage.get(City, city_id)
+    if state is None:
         abort(404)
     if not request.is_json:
         abort(400, description="Not a JSON")
-    data = request.get_json()
-    # Update the City object with new data, excluding protected attributes
+    data = request.get_json(silent=True)()
+    if data is None:
+        abort(400, description="Not a JSON")
     for key, value in data.items():
-        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city, key, value)
-    storage.save()
-    return jsonify(city.to_dict()), 200
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
