b619509d6c9ced8cd11fc0eff0718699a835a9e4
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 21:20:47 -0600

    pep

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,16 @@ from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     """Closes the current SQLAlchemy session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
