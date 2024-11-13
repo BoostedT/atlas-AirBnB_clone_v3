ccdb06beb2856ac54612037d6f635715f27b3a5b
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 23:29:35 -0600

    11

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
-CORS(app, origins="0.0.0.0")
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
