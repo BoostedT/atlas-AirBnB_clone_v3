4dbf3dbeae75b04d223c46a9b17f9c6b8fcea886
Tyler Whitchurch <168224788+BoostedT@users.noreply.github.com>
2024-11-13 16:42:32 -0600

    update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,17 +12,24 @@ from models.user import User
                  strict_slashes=False)
 def get_place_reviews(place_id):
     """Retrieves the list of all Review objects of a Place"""
-    place = storage.get(Place, 'place_id')
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = [review.to_dict() for review in place.reviews]
+    reviews = []
+    for review in storage.all(Review).values():
+        if review.place_id == place_id:
+            reviews.append(review.to_dict())
     return jsonify(reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def get_review(review_id):
     """Retrieves a Review object"""
-    review = storage.get(Review, review_id)
+    review = None
+    for r in storage.all(Review).values():
+        if r.id == review_id:
+            review = r
+            break
     if review is None:
         abort(404)
     return jsonify(review.to_dict())
@@ -32,7 +39,11 @@ def get_review(review_id):
                  strict_slashes=False)
 def delete_review(review_id):
     """Deletes a Review object"""
-    review = storage.get(Review, review_id)
+    review = None
+    for r in storage.all(Review).values():
+        if r.id == review_id:
+            review = r
+            break
     if review is None:
         abort(404)
     storage.delete(review)
@@ -44,7 +55,11 @@ def delete_review(review_id):
                  strict_slashes=False)
 def create_review(place_id):
     """Creates a Review"""
-    place = storage.get(Place, place_id)
+    place = None
+    for p in storage.all(Place).values():
+        if p.id == place_id:
+            place = p
+            break
     if place is None:
         abort(404)
 
@@ -55,7 +70,11 @@ def create_review(place_id):
     if 'user_id' not in data:
         abort(400, description="Missing user_id")
 
-    user = storage.get(User, data['user_id'])
+    user = None
+    for u in storage.all(User).values():
+        if u.id == data['user_id']:
+            user = u
+            break
     if user is None:
         abort(404)
 
@@ -71,7 +90,11 @@ def create_review(place_id):
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id):
     """Updates a Review object"""
-    review = storage.get(Review, review_id)
+    review = None
+    for r in storage.all(Review).values():
+        if r.id == review_id:
+            review = r
+            break
     if review is None:
         abort(404)
 
