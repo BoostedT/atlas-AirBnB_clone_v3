10ca751eee079f1cda6f616cd79bc85857f429a1
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 22:45:39 -0600

    pep 8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -15,7 +15,8 @@ def list_amenities():
     return jsonify([amenity.to_dict() for amenity in amenities])
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route(
+        '/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Get an amenity"""
     amenity = storage.get(Amenity, amenity_id)
@@ -40,7 +41,8 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route(
+        '/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Update an amenity"""
     amenity = storage.get(Amenity, amenity_id)
@@ -59,7 +61,8 @@ def update_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+        '/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Delete an amenity"""
     amenity = storage.get(Amenity, amenity_id)
