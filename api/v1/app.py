bb4eb0740ce14d3d58a9e082407b4ecc4ac5ab92
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-12 23:28:35 -0600

    1

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,8 @@
 """Flask app that performs actions on the states table in hbtn_0e_0_usa"""
 
 from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+import os
 from api.v1.views import app_views
 from models import storage
 
@@ -10,6 +12,7 @@ app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
@@ -25,4 +28,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host="host", port=port, threaded=True)
