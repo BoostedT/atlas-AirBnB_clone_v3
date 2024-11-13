0f5cd52936f431505f0505f1817fbd0b99383381
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-13 10:01:45 -0600

    pep8

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,7 +11,8 @@ from models.amenity import Amenity
 from models.state import State
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route(
+        '/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -32,7 +33,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+        '/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -43,7 +45,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route(
+        '/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def post_place(city_id):
     """Creates a Place"""
     city = storage.get(City, city_id)
